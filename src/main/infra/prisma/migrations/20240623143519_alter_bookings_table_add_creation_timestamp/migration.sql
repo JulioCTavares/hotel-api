13ee189c245101c11fdc9a4e995f97b2030dee12
Julio Tavares <juliotavares197@gmail.com>
2024-06-23 11:35:43 -0300

    chore: add createdAt on bookings table

---
+++ src/main/infra/prisma/migrations/20240623143519_alter_bookings_table_add_creation_timestamp/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `updated_at` to the `bookings` table without a default value. This is not possible if the table is not empty.
+
+*/
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[bookings] ADD [created_at] DATETIME2 NOT NULL CONSTRAINT [bookings_created_at_df] DEFAULT CURRENT_TIMESTAMP,
+[updated_at] DATETIME2 NOT NULL;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
